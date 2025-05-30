62b7b2c7da9dfbe1e34001e2590d91f01556776f
Vibaksanna <vibak011@gmail.com>
2025-05-30 11:00:52 +0700

    done

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'pages/landing_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Worktency',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: LandingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
